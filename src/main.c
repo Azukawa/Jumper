9b2eec00013ef4226250216feb1d04aaa7cd240f
Alero <alero@Ashanti.local>
2026-02-04 00:24:48 +0200

    Testing collision with proto_floor function. Walls and ceiling now have collision.

--- src/main.c
+++ src/main.c
@@ -150,18 +150,13 @@ static inline t_point	update_player_velocity(t_jump *jump, t_point player_vel, i
 	t_point	target_speed = {0, 0};
 
 	if(jump->k.u)
-	{
-		target_speed.y = -top_velocity;
-		player_vel.y = approach(player_vel.y, -top_velocity, 128);
-	}
-	else if(jump->k.d)
-		target_speed.y = top_velocity;
+		player_vel.y = approach(player_vel.y, -top_velocity, 64);
+//	if(jump->k.d)
 	if(jump->k.l)
 		target_speed.x = -top_velocity;
 	else if(jump->k.r)
 		target_speed.x = top_velocity;
 
-//	player_vel.y = approach(player_vel.y, target_speed.y, speed);
 	player_vel.x = approach(player_vel.x, target_speed.x, speed);
 
 	return (player_vel);
@@ -187,13 +182,33 @@ t_point	point_add(t_point a, t_point b)
 	return (ret);
 }
 
+//	if we go under the floor, set height to floor and velocity.y to zero
 t_point		proto_floor(t_point player_pos, t_point *player_vel)
 {
-	if (player_pos.y > 200 << 4)
+	if (player_pos.y > 100 << 4)
 	{
-		player_pos.y = 200 << 4;
+		player_pos.y = 100 << 4;
 		player_vel->y = 0;
 	}
+	if (player_pos.y < -(150 << 4) + (8 << 4))
+	{
+		player_pos.y = -(150 << 4) + (8 << 4);
+		player_vel->y = -player_vel->y >> 1;
+	}
+
+	if (player_pos.x < -(200 << 4) + (8 << 4))
+	{
+			
+		player_pos.x = -(200 << 4) + (8 << 4);
+		player_vel->x = -player_vel->x >> 1;
+	}
+	if (player_pos.x > (200 << 4) - (9 << 4))
+	{
+			
+		player_pos.x = (200 << 4) - (9 << 4);
+		player_vel->x = -player_vel->x >> 1;
+	}
+
 	return (player_pos);
 }
 
@@ -213,12 +228,12 @@ void game_logic(t_rend *rend, t_jump *jump)
 	int		top_velocity		= 128; // this should be divideble by accel to avoid stutter
 
 	player_vel 		= update_player_velocity(jump, player_vel, accel, top_velocity);
-	player_pos 		= point_add(player_pos, player_vel);
 	player_vel		= gravity(player_vel);
+	player_pos 		= point_add(player_pos, player_vel);
 	player_pos		= proto_floor(player_pos, &player_vel);
 	rend_player_pos = world_point_to_rend_point(player_pos);
 
-	draw_circle(rend->win_buffer, rend_player_pos, 15, 0xFFFFFFFF);	
+	draw_circle(rend->win_buffer, rend_player_pos, 8, 0xFFFFFFFF);	
 }
 
 static void	loop(t_rend *rend, SDL_Event *e, t_jump *jump)
