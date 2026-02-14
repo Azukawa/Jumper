bbf7dfb849bb3432d547cf5d02f714a77dff30f8
Alero <alero@Ashanti.local>
2026-02-15 00:34:42 +0200

    spear is once again in play, but does not yet work well with the generic collision function for objects

--- src/main.c
+++ src/main.c
@@ -567,7 +567,7 @@ t_obj init_player()
 	player.vel 		= (t_point){0, 0};
 	player.pos 		= (t_point){256, 256};
 	player.rend_pos	= (t_point){0, 0};
-	player.size 	= (t_point){17, 17};
+	player.size 	= (t_point){16, 16};
 	player.dir 		= 0;
 	player.type 	= TYPE_PLAYER;
 	player.max_jumps= 1;
@@ -581,7 +581,7 @@ t_obj init_spear()
 	t_obj spear;
 
 	bzero(&spear, sizeof(t_obj));
-	spear.pos		= (t_point){1000, -1000};
+	spear.pos		= (t_point){256, 256};
 	spear.vel		= (t_point){0, 0};
 	spear.rend_pos 	= (t_point){0, 0};
 	spear.size		= (t_point){40, 1};
@@ -597,13 +597,14 @@ void	spear_logic(t_jump *jump)
 		jump->spear.vel		= gravity(jump->spear.vel);
 	jump->spear.pos			= point_add(jump->spear.pos, jump->spear.vel);
 							  spear_interaction(jump);	
-							  collision(&jump->spear);
-	jump->spear.rend_pos	= world_point_to_rend_point(jump->spear.pos);
+//							  collision(&jump->spear);
+							terrain_collision(&jump->spear, &jump->map);
 }
 
-void	draw_spear(t_rend *rend, t_jump *jump)
+void	draw_spear(t_rend *rend, t_obj *spear, t_camera *camera)
 {
-	draw_line(rend->win_buffer, (t_point){jump->spear.rend_pos.x - (jump->spear.size.x >> 1), jump->spear.rend_pos.y}, (t_point){jump->spear.rend_pos.x + (jump->spear.size.x >> 1), jump->spear.rend_pos.y}, 0xFFFFFFFF);	
+	spear->rend_pos	= world_point_to_rend_point(point_sub(spear->pos, camera->pos));
+	draw_line(rend->win_buffer, (t_point){spear->rend_pos.x - (spear->size.x >> 1), spear->rend_pos.y}, (t_point){spear->rend_pos.x + (spear->size.x >> 1), spear->rend_pos.y}, 0xFFFFFFFF);	
 }
 
 void	draw_terrain(t_rend *rend, t_map *map, t_point *camera)
@@ -635,21 +636,30 @@ void	terrain_collision(t_obj *obj, t_map *map)
 	int	tile_world_size = tile_rend_size << 4;
 	t_point	tile_size = {tile_world_size, tile_world_size};
 	t_point	map_origo = {-1280, 0};
+	int	half_obj_w = ((obj->size.x >> 1) << 4); 
+//	int	half_obj_w = (obj->size.x << 3); 
+	if(half_obj_w < 1)
+		half_obj_w = 1;
+	int	half_obj_h = ((obj->size.y >> 1) << 4);	// change this to << 3 
+//	int	half_obj_h = (obj->size.y  << 3);	// change this to << 3 
+	if(half_obj_h < 1)
+		half_obj_h = 1;
 	int y = (((obj->pos.y) - map_origo.y) / tile_world_size);
 	int x = (((obj->pos.x) - map_origo.x) / tile_world_size);
-	int left_x = (((obj->pos.x - 128) - map_origo.x) / tile_world_size);
-	int right_x = (((obj->pos.x + 128 - 1) - map_origo.x) / tile_world_size);
-	int head_y = (((obj->pos.y - 128) - map_origo.y) / tile_world_size);
-	int feet_y = (((obj->pos.y + 128) - map_origo.y) / tile_world_size);
+	int left_x = (((obj->pos.x - half_obj_w) - map_origo.x) / tile_world_size);
+	int right_x = (((obj->pos.x + half_obj_w - 1) - map_origo.x) / tile_world_size);
+	int head_y = (((obj->pos.y - half_obj_h) - map_origo.y) / tile_world_size);
+	int feet_y = (((obj->pos.y + half_obj_h) - map_origo.y) / tile_world_size);
 	t_point	a = point_add((t_point){tile_size.x * x, tile_size.y * y}, map_origo);
 	t_point	b = point_add(a, tile_size);
 	//	Floor
 	if	(obj->vel.y > 0 && \
 		x < map->x && x >= 0 && \
 		feet_y < map->y && feet_y >= 0 && \
-		(map->map[left_x + feet_y * map->x] == 'X' || map->map[right_x + feet_y * map->x] == 'X' )) 
+		(map->map[left_x + feet_y * map->x] == 'X' || map->map[right_x + feet_y * map->x] == 'X' || map->map[x + feet_y * map->x] == 'X' )) 
 	{
-		obj->pos.y = a.y + 128;
+		obj->pos.y = a.y + half_obj_h;
+//		obj->pos.y = a.y;
 		obj->vel.y = 0;
 		if(obj->type == TYPE_PLAYER)
 			obj->jumps = obj->max_jumps;
@@ -659,7 +669,7 @@ void	terrain_collision(t_obj *obj, t_map *map)
 		x < map->x && x >= 0 && head_y < map->y && head_y >= 0 && \
 		map->map[x + head_y * map->x] == 'X') 
 	{
-		obj->pos.y = a.y + 128;
+		obj->pos.y = a.y + half_obj_h;
 		obj->vel.y = -obj->vel.y >> 1;
 	}
 	//	Left wall
@@ -667,16 +677,29 @@ void	terrain_collision(t_obj *obj, t_map *map)
 		left_x < map->x && left_x >= 0 && y < map->y && y >= 0 && \
 		map->map[left_x + y * map->x] == 'X') 
 	{
-		obj->pos.x = b.x -128;
-		obj->vel.x = -(obj->vel.x >> 1);
+		if (obj->type == TYPE_SPEAR && abs(obj->vel.x) > 100) // if thrown fast, spear gets stuck
+		{
+			obj->stuck = TRUE;
+			obj->vel = (t_point){0, 0};
+		}
+		else									
+			obj->vel.x = -(obj->vel.x >> 1);
+		obj->pos.x = b.x - half_obj_w;
 	}
 	// Right wall
 	if	(obj->vel.x > 0 && \
 		right_x < map->x && right_x >= 0 && y < map->y && y >= 0 && \
 		map->map[right_x + y * map->x] == 'X') 
 	{
-		obj->pos.x = a.x +128;
-		obj->vel.x = -(obj->vel.x >> 1);
+		if (obj->type == TYPE_SPEAR && abs(obj->vel.x) > 100)
+		{
+			obj->stuck = TRUE;
+			obj->vel = (t_point){0, 0};
+		}
+		else
+			obj->vel.x = -(obj->vel.x >> 1);
+	//	obj->pos.x = a.x + half_obj_w;
+		obj->pos.x = b.x - half_obj_w;
 	}
 //	printf("x = %d\ty = %d\tpos.x = %d\tpos.y = %d\n", x, y, obj->pos.x, obj->pos.y);
 }
@@ -734,8 +757,8 @@ void	game_logic(t_rend *rend, t_jump *jump)
 	draw_player(rend, &jump->player, &jump->camera);
 	draw_terrain(rend, &jump->map, &jump->camera.pos);	
 	cape(rend, jump->player.pos, jump->player.vel, jump->camera.pos);
-//	spear_logic(jump);
-//	draw_spear(rend, jump);
+	spear_logic(jump);
+	draw_spear(rend, &jump->spear, &jump->camera);
 	clear_input_masks(&jump->fresh_keys, &jump->press_keys);
 }
 
